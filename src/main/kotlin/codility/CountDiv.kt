0cbb8b3db5de99b8b73e0909bed4a993e7af38b3
SungJin-Jin <deskhi@nethru.co.kr>
2018-07-21 15:42:56 +0900

    Refactor CountDiv

--- src/main/kotlin/codility/CountDiv.kt
+++ src/main/kotlin/codility/CountDiv.kt
@@ -1,41 +1,37 @@
-package codility
-
-import jdk.vm.ci.code.CodeUtil.K
-
-
+package main.kotlin.codility
 
 /*
-Write a function:
+    Write a function:
 
-class Solution { public int solution(int A, int B, int K); }
+    class Solution { public int solution(int A, int B, int K); }
 
-that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+    that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
 
-{ i : A ≤ i ≤ B, i mod K = 0 }
+    { i : A ≤ i ≤ B, i mod K = 0 }
 
-For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+    For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
 
-Assume that:
+    Assume that:
 
-A and B are integers within the range [0..2,000,000,000];
-K is an integer within the range [1..2,000,000,000];
-A ≤ B.
-Complexity:
+    A and B are integers within the range [0..2,000,000,000];
+    K is an integer within the range [1..2,000,000,000];
+    A ≤ B.
+    Complexity:
 
-expected worst-case time complexity is O(1);
-expected worst-case space complexity is O(1).
+    expected worst-case time complexity is O(1);
+    expected worst-case space complexity is O(1).
 
-coverage 100%
+    coverage 100%
  */
 fun main(args: Array<String>) {
     solution(6, 11, 2)
 }
 
-fun solution(A: Int, B: Int, K: Int) {
-    var value = B / K - A / K
+fun solution(start: Int, end: Int, divisor: Int) {
+    var value = (end - start) / divisor
 
-    if (A % K == 0)
-        value++
-    else
-        value
+    when {
+        start % divisor == 0 -> value++
+        else -> value
+    }
 }
