b41f77b774ea0f3c7de3934d18adedc54d2e7063
SungJin-Jin <deskhi@nethru.co.kr>
2018-07-21 21:31:25 +0900

    Fix require code at Distinct.kt

--- src/main/kotlin/codility/Distinct.kt
+++ src/main/kotlin/codility/Distinct.kt
@@ -3,7 +3,7 @@ package main.kotlin.codility
 fun main(args: Array<String>) {
 
     require(3 == solve(arrayOf(2, 1, 1, 2, 3, 1)))
-    require(4 == solve(arrayOf(2, 1, 1, 2, 3, 1)))
+    require(4 == solve(arrayOf(3, 4, 1, 2, 3)))
 }
 
 private fun solve(values: Array<Int>): Int {
