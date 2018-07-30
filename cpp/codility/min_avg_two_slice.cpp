07af9653caffdb39935e880a56711059a28f836c
jj-pa <krap9482@gmail.com>
2018-07-30 14:49:40 +0900

    [init] algorithm homework..!
    * MaxCounters
    * CountDiv
    * MinAvgTwoSlice
    
    https://app.codility.com/programmers/lessons/4-counting_elements/
    https://app.codility.com/programmers/lessons/5-prefix_sums/

---
+++ cpp/codility/min_avg_two_slice.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> &A)
+{
+	double prev, calc;
+	int result;
+	int start, end;
+	int temp;
+	int sum;
+
+	prev = (A.at(0) + A.at(1)) / 2;
+	for (start = 0; start < A.size() - 1; start++) {
+		for (end = start + 1; end <= A.size() - 1; end++) {
+			temp = start;
+			sum = 0;
+			while (temp <= end) {
+				sum += A.at(temp);
+				temp++;
+			}
+			calc = (double)sum / (end - start + 1);
+			if (calc < prev) {
+				prev = calc;
+				result = start;
+			}
+		}
+	}
+
+	return result;
+}
